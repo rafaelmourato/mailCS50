a5eeacea349af6b2a1931feea0e784ffdaffe956
Rafael Mourato <rafaelmourato@MacBook-Air-de-Rafael-2.local>
2025-09-03 22:03:35 -0300

    Migrations made, read.me added

--- mail/admin.py
+++ mail/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import User, Email
+
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Email)
+# mail examples: emal senha: rafinha6%$
+# harvard harvardmail ou harvardmail50
